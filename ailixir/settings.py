1a43d86d3982623c3927020c7fc97b396ee8b360
Jafaru Idris <jafaridris@gmail.com>
2025-10-28 20:42:15 +0100

    load env

--- ailixir/settings.py
+++ ailixir/settings.py
@@ -11,14 +11,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Locate and load your .env file manually
+dotenv_path = os.path.join(BASE_DIR, '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
